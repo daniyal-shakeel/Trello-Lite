06bf7f2f3f2946491cc741d9a51c8701e5217766
Malik <hafizdaniyalshakeel@gmail.com>
2025-10-15 19:55:02 +0500

    app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [process.env.CLIENT_URI],
+    origin: [...process.env.ALLOWED_ORIGINS.split(",")],
     credentials: true,
   })
 );
